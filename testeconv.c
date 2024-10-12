964d6d62ca0ab80a99ade9b05e3fefa0fc742286
MaluDutra <mldutraga@gmail.com>
2024-10-12 18:35:08 -0300

    testes

--- testeconv.c
+++ testeconv.c
@@ -12,8 +12,8 @@ int main(void){
     fclose(arquivo_entrada);
     fclose(arquivo_saida);
 
-    arquivo_entrada = fopen("utf8_grande.txt", "rb");
-    arquivo_saida = fopen("utf32_conv_grande.txt", "wb");
+    arquivo_entrada = fopen("arquivo_utf8.txt", "rb");
+    arquivo_saida = fopen("aarquivo_utf32_conv.txt", "wb");
     
     if (convUtf8p32(arquivo_entrada, arquivo_saida) == 0){
         printf("2 - Texto decodificado de UTF-8 para UTF-32 com sucesso!\n");
@@ -21,6 +21,39 @@ int main(void){
 
     fclose(arquivo_entrada);
     fclose(arquivo_saida);
+
+    arquivo_entrada = fopen("arquivo_utf8_complex.txt", "rb");
+    arquivo_saida = fopen("arquivo_utf32_le_bom_complex_conv.txt", "wb");
+    
+    if (convUtf8p32(arquivo_entrada, arquivo_saida) == 0){
+        printf("3 - Texto decodificado de UTF-8 para UTF-32 com sucesso!\n");
+    }
+
+    fclose(arquivo_entrada);
+    fclose(arquivo_saida);
+
+    printf("Conversão de UTF-32 para UTF-8 para UTF-32 de volta\n");
+
+    arquivo_entrada = fopen("utf32_peq.txt", "rb");
+    arquivo_saida = fopen("utf32_p_utf8_peq1.txt", "wb");
+    
+    if (convUtf32p8(arquivo_entrada, arquivo_saida) == 0){
+        printf("Texto decodificado de UTF-32 para UTF-8 com sucesso!\n");
+    }
+
+    fclose(arquivo_entrada);
+    fclose(arquivo_saida);
+
+    arquivo_entrada = fopen("utf32_p_utf8_peq1.txt", "rb");
+    arquivo_saida = fopen("utf8_p_utf32_peq1.txt", "wb");
+    
+    if (convUtf8p32(arquivo_entrada, arquivo_saida) == 0){
+        printf("Texto decodificado de UTF-8 para UTF-32 com sucesso!\n");
+    }
+
+    fclose(arquivo_entrada);
+    fclose(arquivo_saida);
+
 
     return 0;
 }
